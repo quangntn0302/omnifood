ea374386e1b2a096c8f552ddec803fc967dd8485
Quang Nguyen <quangntn0302@gmail.com>
2019-07-27 14:01:31 +0700

    update navigation mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -82,4 +82,18 @@ $(document).ready(function() {
         offset: '50%;'
     });
 
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+        }
+    });
+
 });
